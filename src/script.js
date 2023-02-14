d4f26b7f1125ebb0696deeb786a533d58a0fbf5a
EidanU <eidanughetti.contact@gmail.com>
2023-02-14 12:04:27 +0100

    [WIP] add planet rotation arround sun

--- src/script.js
+++ src/script.js
@@ -119,24 +119,6 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 
-function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
-    pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
-
-    if(pointIsWorld){
-        obj.parent.localToWorld(obj.position); // compensate for world coordinate
-    }
-
-    obj.position.sub(point); // remove the offset
-    obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
-    obj.position.add(point); // re-add the offset
-
-    if(pointIsWorld){
-        obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
-    }
-
-    obj.rotateOnAxis(axis, theta); // rotate the OBJECT
-}
-
 const sunAxis = new THREE.Vector3( sun.planet.position.x, sun.planet.position.y, sun.planet.position.z)
 const clock = new Clock();
 
@@ -174,6 +156,5 @@ const tick = () => {
     });
 }
 
-tick()
-
+tick();
 
